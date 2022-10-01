b25c0ef5ec674925740f28fb9a6c461739c0d0dd
Candice Kim <87719374+CandiceKim@users.noreply.github.com>
2022-09-30 20:24:00 -0400

    add server connection

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+
+const path = require("path");
+
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(process.cwd() + "/about.html");
+});
+
+app.get("/connect", (req, res) => {
+    res.sendFile(process.cwd() + "/connect.html");
+});
+
+app.listen(3002, () => {
+    console.log("Server started at port 3002");
+});
